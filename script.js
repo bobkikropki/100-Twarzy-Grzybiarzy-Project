7244186a3872b0f63cde20a4d84407dfddfc17da
venekq <dawidzmijewski0123@gmail.com>
2026-03-23 14:33:33 +0100

    lista i wyszukiwarka

--- script.js
+++ script.js
@@ -36,3 +36,20 @@ const demoElem = document.getElementById("demo");
 if (demoElem) {
   demoElem.innerHTML = "Hello, JavaScript!";
 }
+
+
+function dropDownList() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
