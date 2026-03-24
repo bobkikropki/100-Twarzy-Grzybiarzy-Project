5e6f87a33c86dc365c31c98964df0bfb879415e2
Jan <janusz.mazz1@gmail.com>
2026-03-24 15:18:48 +0100

    dodanie js funkcji dawida

--- script.js
+++ script.js
@@ -28,3 +28,18 @@ async function getID()
   }
   }
 
+function dropDownList() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
